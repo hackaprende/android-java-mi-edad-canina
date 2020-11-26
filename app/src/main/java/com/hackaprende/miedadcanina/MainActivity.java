88842a92d3ea61655a3214d5c5c65a1de18fe34e
Jesus Almaral <almaral17@gmail.com>
2020-11-25 18:24:14 -0600

    Add app logic

--- app/src/main/java/com/hackaprende/miedadcanina/MainActivity.java
+++ app/src/main/java/com/hackaprende/miedadcanina/MainActivity.java
@@ -1,8 +1,11 @@
 package com.hackaprende.miedadcanina;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText ageEdit = findViewById(R.id.age_edit);
+        TextView resultText = findViewById(R.id.result_text);
+        Button button = findViewById(R.id.button);
+
+        button.setOnClickListener(v -> {
+            String age = ageEdit.getText().toString();
+            int ageInt = Integer.parseInt(age);
+
+            int result = ageInt * 7;
+            String resultString = "Si fueras perro, tu edad sería " + result + " años";
+            resultText.setText(resultString);
+        });
     }
 }
