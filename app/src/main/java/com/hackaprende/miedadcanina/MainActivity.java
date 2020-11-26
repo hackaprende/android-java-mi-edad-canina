3320dac55d172c176bf9012fbbc6ed8923e3284b
Jesus Almaral <almaral17@gmail.com>
2020-11-26 13:00:48 -0600

    Add a toast when age is empty

--- app/src/main/java/com/hackaprende/miedadcanina/MainActivity.java
+++ app/src/main/java/com/hackaprende/miedadcanina/MainActivity.java
@@ -1,9 +1,11 @@
 package com.hackaprende.miedadcanina;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -20,11 +22,16 @@ public class MainActivity extends AppCompatActivity {
 
         button.setOnClickListener(v -> {
             String age = ageEdit.getText().toString();
-            int ageInt = Integer.parseInt(age);
+            if (!age.isEmpty()) {
+                int ageInt = Integer.parseInt(age);
 
-            int result = ageInt * 7;
-            String resultString = "Si fueras perro, tu edad sería " + result + " años";
-            resultText.setText(resultString);
+                int result = ageInt * 7;
+                String resultString = "Si fueras perro, tu edad sería " + result + " años";
+                resultText.setText(resultString);
+            } else {
+                Log.d("MainActivity", "Age field is empty");
+                Toast.makeText(this, "Debes insertar una edad", Toast.LENGTH_SHORT).show();
+            }
         });
     }
 }
